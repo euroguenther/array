48eef058a7f68b8d637ce219516df758a30022e4
orockstroh <orockstroh@PC04146578>
2017-02-23 10:16:38 +0100

    Deklaration und Initialisierung eindimesional int arrays

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -11,6 +11,39 @@ int main()
 	// Pointer
 
 
+	//---- Eindimensionale int arrays
+
+
+	// eindimesionale Arrays werden mit eckigen Klammern definiert
+	//int aZahl[4];
+	// eindimesionale Arrays beschreiben
+	// Kann dies explizit geschehen oder implizit
+	// Schreiben in arrays immer über den Index der Index beginnt immer bei 0
+	// Arrays dürfen niemals überladen werden
+
+
+	//Explizit
+	//aZahl[0] = 10;
+	//aZahl[1] = 20;
+	//aZahl[2] = 30;
+	//aZahl[3] = 40;
+
+
+	// Implizit
+
+	int aZahl[4] = {10,20,30,40};
+
+
+	int a = 1;
+
+	// zugreifen auf skalarwert arrays nur über index möglich
+
+	printf("%i", aZahl[3]);
+
+
+
+
+
 
 	
 	

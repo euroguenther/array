2b506cb93150b8c5fdb4cc50a1aad07c080afaac
orockstroh <orockstroh@PC04146578>
2017-02-23 09:22:05 +0100

    initiales Commit

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+// ConsoleApplication1.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include <stdio.h>
+
+
+int main()
+{
+    
+	// Ein- und Mehrdimensionale Arrays
+	// Pointer
+
+
+
+	
+	
+	
+	
+	return 0;
+}
+
